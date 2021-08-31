945a0c9d69e2689d131718147683690be1d74476
StanislWka <stas-babitsky@mail.ru>
2021-08-31 14:16:02 +0300

    HM Mvc-concept

--- 2021_Stanislav/Events/events.js
+++ 2021_Stanislav/Events/events.js
@@ -6,40 +6,70 @@ img.src = "https://images.unsplash.com/photo-1630278107980-46fde63cdc12?ixid=Mnw
 
 // onload
 img.onload = function() {
-  console.log(`Изображение загружено, размеры ${img.width}x${img.height}`);
+  // console.log(`Изображение загружено, размеры ${img.width}x${img.height}`);
 };
 
 // onerror
 img.onerror = function() {
-    console.log("Ошибка во время загрузки изображения");
+    // console.log("Ошибка во время загрузки изображения");
 };
 
 // onresize
-window.addEventListener('resize', function() {
-    console.log(`${window.innerWidth}x${window.innerHeight}`)
-}, true);
+// window.addEventListener('resize', function() {
+//     // console.log(`${window.innerWidth}x${window.innerHeight}`)
+// }, true);
 
 // scroll
+const menuElem = document.querySelector('.menu');
+
+// window.addEventListener('scroll', () => {
+//     menuElem.classList.toggle('is_fixed', window.pageYOffset >= 255)
+//     // if(window.pageYOffset >=255){
+//     //     menuElem.classList.add('is_fixed')
+//     // }else{
+//     //     menuElem.classList.remove('is_fixed')
+//     // }
+// })
+
+
 
 // ==================================
 // Mouse events
 // ==================================
 
+const mouseEventElement = document.getElementById('mouseEvents');
+
+function PrintEvent (event){
+    console.log(event);
+}
+
 // click, onclick
-
-// mouseover, onmouseover
-
-// mouseout, onmouseout
-
-// mousedown, onmousedown
-
-// mouseup, onmouseup
+// mouseEventElement.onclick = PrintEvent;
+//
+// // mouseover, onmouseover
+// mouseEventElement.onmouseover = PrintEvent;
+//
+// // mouseout, onmouseout
+//
+// mouseEventElement.onmouseout = PrintEvent;
+//
+// // mousedown, onmousedown
+//
+// mouseEventElement.onmousedown = PrintEvent;
+//
+// // mouseup, onmouseup
+//
+// mouseEventElement.onmouseup= PrintEvent;
 
 // mousemove, onmousemove
+
+// mouseEventElement.onmousemove = PrintEvent;
 
 // ==================================
 // Keyboard events
 // ==================================
+
+// document.addEventListener('keyup', console.log)
 
 // onkeydown & onkeyup
 
@@ -47,15 +77,57 @@ window.addEventListener('resize', function() {
 // Form events
 // ==================================
 
+const userNAmeInput = document.querySelector('[name="username"]')
+
 // focus, onfocus
 
-// submit, onsubmit
+userNAmeInput.focus();
+
+userNAmeInput.onfocus = console.log;
+
+
 
 // blur, onblur
 
+// userNAmeInput.onblur = function (event){
+//     if(!event.target.value.trim()){
+//         alert("Inavlid");
+//     }
+// }
+
 // change, onchange
+
+userNAmeInput.onchange = function (event){
+    console.log('onchage', event.target.value);
+}
+
+
+// submit, onsubmit
+
+const form = document.querySelector('form');
+form.onsubmit = function (event){
+    event.preventDefault();
+    console.log(event);
+}
 
 // ==================================
 // MutationObserver
 // ==================================
+
+const placeHolderElemnts = document.getElementById('placeholder');
+const targetNode = document.body;
+
+const  config = {attributes: true, childList: true, subtree: true};
+
+const callback = function (muttationList, pbserved){
+    console.log(muttationList);
+}
+
+const  observed = new MutationObserver(callback);
+
+observed.observe(targetNode, config);
+
+setTimeout(() =>{
+    placeHolderElemnts.innerHTML = "muttation Observed";
+}, 2000);
 
